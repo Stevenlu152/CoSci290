eda542b88db023fd36d1150d54ef42d859f42003
stevenlu40142 <stevenlu40142@gmail.com>
2018-03-08 17:37:40 -0500

    Quiz3

---
+++ Quiz3B.java
@@ -0,0 +1,42 @@
+/* 
+  Steven Lu
+  
+  Co Sci 290 
+  
+  question 1.9 & 1.10
+  
+*/ 
+
+import java.util.Scanner;
+public class Quiz3B{
+  
+  public static void main(String[] args){
+    
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("We're using this formula to find the Area \n "
+                      + "Area = width x height \n"
+                      + "Using Width 4.5 and height 7.9");
+    
+    System.out.println(" A = 4.5 x 7.9 ");
+    
+    System.out.println(4.5 * 7.9 );   
+
+/* 
+  #
+  1.10
+  
+*/
+
+  System.out.println("D=R/T \n"
+                    + "R = 8.6992 miles / Time 45min and 30 sec");
+  System.out.println (8.6992/45.5);
+    
+    
+    } 
+  }
+    
+
+
+
+
