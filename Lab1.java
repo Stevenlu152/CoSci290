8f4a72461073d133449b8cc6d74052cd72e7c13c
stevenlu40142 <stevenlu40142@gmail.com>
2018-05-31 17:21:12 -0400

    Lab1

---
+++ Lab1.java
@@ -0,0 +1,30 @@
+/* 
+  Steven Lu 
+  Co sci 290 
+  2/13/2018
+*/
+
+public class Lab1{
+
+  public static void main(String[] args){
+    
+    System.out.print("oOoOOoOOo  o                o          `O     o      o \n"
+                    + "     o     O                 O           o o  O      O  \n"
+                    + "     o     o                 o           O    o      o  \n"
+                    + "     O     O                 O           O    O      o  \n"
+                    + "     o     OoOo. .oOo.       o     o     o O  o  .oOoO \n"  
+                    + "     O     o   o OooO'       O     O     O o  O  o   O \n" 
+                    + "     O     o   O O           `o   O o   O' O  o  O   o \n" 
+                    + "     o'    O   o `OoO'        `OoO' `OoO'  o' Oo `OoO'o \n"
+                                                       
+                                                       
++ "OooOOo.   o                            .oOOOo.                       o    \n"
++ "O     `O O                            .O     o                      O     \n"
++ "o      O o                            o                             o     \n"
++ "O     .o O                            o                             O     \n"
++ "oOooOO'  o  .oOoO' 'OoOo. .oOo.       o         `OoOo. .oOoO' .oOo  OoOo. \n"
++ "o        O  O   o   o   O OooO'       O          o     O   o  `Ooo. o   o \n"
++ "O        o  o   O   O   o O           `o     .o  O     o   O      O o   O \n"
++ "o'       Oo `OoO'o  o   O `OoO'        `OoooO'   o     `OoO'o `OoO' O   o \n");
+  }
+}
