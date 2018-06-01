59b17d1c1809240ae18e1867d258ed7a88e0ea62
stevenlu40142 <stevenlu40142@gmail.com>
2018-06-01 02:34:04 -0400

    Lab21

--- lab21.java
+++ lab21.java
@@ -1,4 +1,18 @@
-*/
+/*
   Steven Lu
   Co sci 290
-/*
+*/
+
+public class Lab21{
+  public static void main(String[] args){
+    
+    int[] single = new int[10];
+    
+    for (int i = 1; i <= 100; i++) {
+	    single[(int)(Math.random() * 10)]++;
+    }
+      for (int i = 0; i < single.length; i++) {
+	    System.out.println(i + "s: " + single[i]);
+      }
+  }
+}
