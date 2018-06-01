42523195dec20dffdd34b559b40c1f15414be0f4
stevenlu40142 <stevenlu40142@gmail.com>
2018-06-01 00:52:28 -0400

    DataEscape Splash

---
+++ DataEscape.java
@@ -0,0 +1,30 @@
+/* 
+  Steven Lu
+  Co sci 290 
+*/ 
+
+public class DataEscape{
+  
+  public static void main(String[] args){
+    
+    System.out.println(
+  "o.OOOo.                                             o          \n"
++ "O    `o                     o                     O            \n"
++ "o      O                                      O   o           \n"
++ "O      o                                     oOo  O           \n"
++ "o      O .oOoO' O   o       O  'OoOo.         o   OoOo. .oOo. \n"
++ "O      o O   o  o   O       o   o   O         O   o   o OooO' \n"
++ "o    .O' o   O  O   o       O   O   o         o   o   O O     \n"
++ "OooOO'   `OoO'o `OoOO       o'  o   O         `oO O   o `OoO' \n"
+  + "                  o                                         \n"
+  + "               OoO'                                         \n"
+ + "o.OOOo.                                                      \n"
+ + "O    `o                                                     \n"
+ + "o      O          O                                         \n"
+ + "O      o         oOo                                        \n"
+ + "o      O .oOoO'   o   .oOoO' .oOo  .oOo  .oOoO' .oOo. .oOo. \n"
+ + "O      o O   o    O   O   o  `Ooo. O     O   o  O   o OooO' \n"
+ + "o    .O' o   O    o   o   O      O o     o   O  o   O O     \n"
+ + "OooOO'   `OoO'o   `oO `OoO'o `OoO' `OoO' `OoO'o oOoO' `OoO'" );
+  }
+}
