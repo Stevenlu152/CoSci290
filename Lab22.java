ae06ee8646b8a3f78e3c386e530f331c882f8427
stevenlu40142 <stevenlu40142@gmail.com>
2018-06-01 02:42:18 -0400

    Lab22

---
+++ Lab22.java
@@ -0,0 +1,41 @@
+/* 
+  Steven Lu
+  Co sci 290
+*/
+
+public class Lab22{
+  
+  public static void main(String[] args){
+    
+    int[] number = new int[200];
+    
+    
+    for(int i = 0; i < 14; i++)
+    {
+      number[i] = (int)(Math.random() * (14 - 0) + 1);
+    }
+       for(int i = 0; 1 < 14; i++)
+       {
+      System.out.println(number[i] + " ");
+       }
+
+     for(int i = 1; i < 14; i++) 
+     {
+      if (i % 2 != 0) 
+      {
+	      System.out.println(" odd : " + i);
+	    }
+     }
+     for(int i = 1; i < 14; i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print("even : " + i);
+            }
+        }   
+    
+    
+      
+  }
+
+}
